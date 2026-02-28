fa927e4a09365594d7c778d26b65263d4a79f700
Rohit Kumar Mishra <105963356+Rohitmishra123@users.noreply.github.com>
2026-02-28 10:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-
+  let form = document.getElementById("form1");
+  let firstName = form.fname.value.trim();
+  let lastName = form.lname.value.trim();
+  if (firstName === "" && lastName === "") {
+    alert("Please enter your name");
+    return false; 
+  let fullName = firstName + " " + lastName;
+  alert(fullName.trim());
+  return false; 
 }
